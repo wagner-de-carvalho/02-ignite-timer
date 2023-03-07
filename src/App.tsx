4b4aba9cfd3df15d177dc43f164a8e8e8b048806
wagner <wagncarv@outlook.com>
2023-03-06 22:35:34 -0300

    Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from './components/Button';
+
 export function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Button variant='primary' />
+      <Button variant='secondary' />
+      <Button variant='danger' />
+      <Button variant='neutral' />
+      <Button />
+    </>
   )
 }
 
---
+++ src/components/Button.styles.ts
@@ -0,0 +1,23 @@
+import styled, { css } from "styled-components";
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'neutral';
+
+interface ButtonContainerProps {
+    variant?: ButtonVariant
+}
+
+const buttonVariant = {
+    primary: 'purple',
+    secondary: 'orange',
+    danger: 'red',
+    success: 'green'
+}
+
+export const ButtonContainer = styled.button < ButtonContainerProps>`
+    width: 100px;
+    height: 40px;
+
+    ${props => {
+        return css`background-color: ${buttonVariant[props.variant]}`
+    }}
+`
