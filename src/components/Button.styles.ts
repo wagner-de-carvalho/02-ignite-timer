962775e13847d4f74f290a47084647307459197d
wagner <wagncarv@outlook.com>
2023-03-07 20:50:31 -0300

    Configurando temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,8 +16,10 @@ const buttonVariant = {
 export const ButtonContainer = styled.button < ButtonContainerProps>`
     width: 100px;
     height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
 
-    ${props => {
-        return css`background-color: ${buttonVariant[props.variant]}`
-    }}
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.white};
 `
