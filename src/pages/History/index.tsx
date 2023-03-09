16467a80fc7890fa23e89a8b679bc8a0145dfc7e
wagner <wagncarv@outlook.com>
2023-03-09 10:08:23 -0300

    Página: History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,53 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>History</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>20 minutos</td>
+              <td>Concluída</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>20 minutos</td>
+              <td>Concluída</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>20 minutos</td>
+              <td>Concluída</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>20 minutos</td>
+              <td>Concluída</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>20 minutos</td>
+              <td>Concluída</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
